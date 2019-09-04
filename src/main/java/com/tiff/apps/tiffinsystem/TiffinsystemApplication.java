daaa6eb52c4dc17fbc9d424c0c4ccfa5ea255a29
nitu verma <nitu.verma@onmobile.com>
2019-09-05 00:39:16 +0530

    Commit of gradle prop

---
+++ src/main/java/com/tiff/apps/tiffinsystem/TiffinsystemApplication.java
@@ -0,0 +1,13 @@
+package com.tiff.apps.tiffinsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiffinsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiffinsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiff/apps/tiffinsystem/TiffinsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tiff.apps.tiffinsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TiffinsystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
